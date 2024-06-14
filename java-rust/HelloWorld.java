f62f0f9db7efc1ce03afd59a07527c9aa3c8759b
unknown <taravirginillo@gmail.com>
2024-06-14 13:05:46 -0700

    fix(main): fix from loadLibrary to load to get java to recognize rust lib

--- java-rust/HelloWorld.java
+++ java-rust/HelloWorld.java
@@ -1,18 +1,14 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class HelloWorld {
     // This declares that the static `hello` method will be provided
     // a native library.
     private native void hello();
 
     static {
-        // This actually loads the shared object that we'll be creating.
-        // The actual location of the .so or .dll may differ based on your
-        // platform.
-        System.setProperty("java.library.path", "C:\\Users\\Tara\\java-jni-bindings\\java-rust\\target\\debug");
-        final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
-        sysPathsField.setAccessible(true);
-        sysPathsField.set(null, null);
-        System.out.println(System.getProperty("java.library.path"));
-        System.loadLibrary("mylib");
+        Path p = Paths.get(String.format("target/debug/%s", System.getProperty("rustlib")));
+        System.load(p.toAbsolutePath().toString());
         System.out.println("Successfully loaded native rust library!");
     }
 
--- java-rust/README.md
+++ java-rust/README.md
@@ -1,1 +1,30 @@
-"# rust-java-jni-binding" 
+# Java <-> Rust JNI Binder
+
+A simple binder to use a Rust method inside of a Java class.
+
+This was based off a tutorial here:
+https://docs.rs/jni/latest/jni/
+
+## Running Your Java HelloWorld Class
+
+To run your Java 'HelloWorld' class with a native Rust method, simply build your Rust library:
+
+`cargo build`
+
+Ensure inside your ./target/debug folder is a .so file, which is the Rust library we want to import.
+
+Compile your Java HelloWorld.java class:
+
+`javac HelloWorld.java`
+
+Add a header file:
+
+`javac -h . HelloWorld.java`
+
+And run your java file, with an argument specifying your rustlib. If you are on Windows, it would be:
+
+`java -Drustlib=mylib.dll HelloWorld`
+
+And if you're on Linux:
+
+`java -Drustlib=libmylib.so HelloWorld`
